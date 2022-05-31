04e91fb93a2964237d029dd58c72b784653a1a14
Tomas Sanchez <tms90@outlook.es>
2022-05-31 18:57:13 +0200

    Added startServer function in index.js/server folder

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const debug = require("debug")("coffeeshops:server");
+const chalk = require("chalk");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { notFoundError, generalError } = require("./middlewares/error");
+
+const app = express();
+
+const startServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.yellow(`Server listening on http://localhost:${port}`));
+      resolve();
+    });
+
+    server.on("error", (error) => {
+      reject(error);
+    });
+  });
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
+
+module.exports = { startServer, app };
