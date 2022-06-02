d3eee6b6ecb7376682aadcbbb70d08893e0e4a04
Tomas Sanchez <tms90@outlook.es>
2022-06-02 22:23:22 +0200

    Added joi userCredentialRegister and login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const { validate } = require("../database/models/UserSchema");
 const {
   userRegister,
   userLogin,
 } = require("../server/controllers/userController");
-const {
-  credentialsRegisterSchema,
-  credentialsLoginSchema,
-} = require("../server/schemas/useCredentials");
 
 const userRouter = express.Router();
 
-userRouter.post("/login", validate(credentialsLoginSchema), userLogin);
-userRouter.post("/register", validate(credentialsRegisterSchema), userRegister);
+userRouter.post("/login", userLogin);
+userRouter.post("/register", userRegister);
 
 module.exports = userRouter;
