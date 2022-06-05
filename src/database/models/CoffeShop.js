7b9bc9f039570cc1fb70f0f6b8b91bc109160757
Tomas Sanchez <tms90@outlook.es>
2022-06-05 13:33:15 +0200

    Added coffeshop Schema in models and coffeshop ontroller

--- src/database/models/CoffeShop.js
+++ src/database/models/CoffeShop.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-
 const CoffeShopSchema = new Schema({
   name: {
     type: String,
@@ -16,7 +15,7 @@ const CoffeShopSchema = new Schema({
     type: Number,
     max: 5,
     min: 0,
-  
+  },
 });
 
 const CoffeShop = model("CoffeShop", CoffeShopSchema, "CoffeShops");
