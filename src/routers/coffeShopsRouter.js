fd9c9fb558ab6641517564f38ecd663a318786cd
Tomas Sanchez <tms90@outlook.es>
2022-06-08 14:14:28 +0200

    Enrouted delete function in CoffeeshopRouter foulder

--- src/routers/coffeShopsRouter.js
+++ src/routers/coffeShopsRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   getCoffeeShops,
+  deleteCoffeShop,
 } = require("../server/controllers/coffeeShopController");
 
 const coffeeShopsRouter = express.Router();
 
 coffeeShopsRouter.get("/list", getCoffeeShops);
+coffeeShopsRouter.delete("/:idCoffeeShop", deleteCoffeShop);
 
 module.exports = coffeeShopsRouter;
